9f84a160e9bc59fb48231284fe52323d83831f55
Adrian Abadin <aabadin@gmail.com>
2023-03-13 16:29:31 -0300

    Trabajhando dao selector

--- services/selectorDAO.ts
+++ services/selectorDAO.ts
@@ -1,17 +1,33 @@
-import { IDAO, IDAOSelector} from '../types';
-const MongoDAO=require('./mongoDAO')
+import { Schema } from 'mongoose';
+import { IDAO, IDAOSelector, IdbConnectionObject, IgoogleUser, IlocalSchema,ImongoDB} from '../types';
+//const MongoDAO=require('./mongoDAO')
+import {MongoDAO} from './mongoDAO'
 const SqlDAO = require('./sqlDAO')
 console.log(SqlDAO)
 class DAOSelector implements IDAOSelector {
+   public database:any
+   static instance:any
    constructor(
-   public MONGO:any =  MongoDAO,
-   public SQL:any = SqlDAO
-   ){}
-    
-}
-const DAOSelectorObject =new DAOSelector()
+      db:Schema<IlocalSchema> | Schema<IgoogleUser> | ImongoDB |IdbConnectionObject,
+      schemaType: "goaSchema" |"localSchema",
+      dbType: "MONGO"|"SQL",
+   protected isLocal = (schemaType==="localSchema"),
+   protected isMongo = (dbType==="MONGO"),
+   protected isSQL = (dbType==="SQL"),
+   protected loadDAO = async()=>{
+      const DAO= new MongoDAO(db as Schema<IlocalSchema> | Schema<IgoogleUser> | ImongoDB ,schemaType)
+      this.database=await DAO.createInstance()
+   }
+   ){
+      if (isMongo) this.database= loadDAO()
+      else if (isSQL) this.database = new SqlDAO(db,schemaType)
+  }
+ }
+ const URL = "mongodb+srv://dcsweb:MopG23GHLEu3GwB0@dcsweb.snm3hyr.mongodb.net/?retryWrites=true&w=majority"
 
-export default DAOSelectorObject
+const DAOSelectorObject =new DAOSelector({db:URL,dbSchema:{phone:{type:Number}}},"localSchema","MONGO")
+console.log(DAOSelectorObject)
+export default DAOSelector
 
 
 
